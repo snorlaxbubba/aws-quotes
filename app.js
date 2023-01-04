65686d90df8799cb9f987e765406dc4fb538d5e0
Mike <113997900+catscancode-portfolio@users.noreply.github.com>
2023-01-04 15:45:12 -0800

    Add the react app

--- app.js
+++ app.js
@@ -1,35 +1,50 @@
 const express = require('express')
 const app = express()
 
-app.get("/", (req, res) => {
-  console.log("GET /")
-  res.send("<h1>hello aws</h1>")
-})
+app.use(express.static('dist'))
 
 app.use(express.json())
 
 const pokemons = [
-  {
-    id: 1,
-    name: "Pikachu",
-    type: "electric ⚡️",
-    level: 99,
-    image: "/pikachu.webp"
-  }
+    {
+      id: 1,
+      name: "Pikachu",
+      type: "electric ⚡️",
+      level: 99,
+    },
+    {
+        id: 2,
+        name: "Umbreon",
+        type: "Dark ⚡️",
+        level: 100,
+    },
+    {
+        id: 3,
+        name: "Pichu",
+        type: "electric ⚡️",
+        level: 9,
+    },
 ]
-
+  
 app.get("/api/pokemons", (req, res) => {
-  console.log("GET /api/pokemons")
-  res.send({pokemons: pokemons})
+console.log("GET /api/pokemons")
+res.send({pokemons: pokemons})
 });
 
 app.post("/api/pokemons", (req, res) => {
-  const data = req.body
-  console.log("POST /api/pokemons", data)
-  data.id = pokemons.length+1
-  pokemons.push(data)
-  res.send(data)
+const data = req.body
+console.log("POST /api/pokemons", data)
+data.id = pokemons.length+1
+pokemons.push(data)
+res.send(data)
 })
+
+
+// After all other routes
+app.get('*', (req, res) => {
+  res.sendFile('dist/index.html');
+});
+
 
 const port = process.env.PORT || 8080
 app.listen(port, () => console.log(`listening on port ${port}`))
